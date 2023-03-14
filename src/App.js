cb31bb9568c99f616d0c93a09a28f6dda53a4105
kandreska <114035801+kandreska@users.noreply.github.com>
2023-03-13 20:40:45 -0500

    Added axios integration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1> Hello from React </h1>
+          <Weather />
         </p>
         <a
           className="App-link"
